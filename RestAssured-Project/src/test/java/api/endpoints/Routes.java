a886f0426496e8b8d3884df341729c74be4eab54
amit <monalibedre@gmail.com>
2025-07-27 13:13:34 +0530

    update file

---
+++ RestAssured-Project/src/test/java/api/endpoints/Routes.java
@@ -0,0 +1,23 @@
+package api.endpoints;
+
+
+
+// swagger uri  --> https://petstore.swagger.io/
+// create post --> https://petstore.swagger.io/v2/user
+//  put----> https://petstore.swagger.io/v2/user/{username}
+// delete--> https://petstore.swagger.io/v2/user/{username}
+
+
+public class Routes {
+	
+	public static String base_url= "https://petstore.swagger.io/v2";
+	
+	//user module
+	public static String post_url=base_url+"/user";
+	public static String get_url=base_url+"/user/{username}";
+	public static String update_url=base_url+"/user/{username}";
+	public static String delete_url=base_url+"/user/{username}";
+	
+	
+
+}
